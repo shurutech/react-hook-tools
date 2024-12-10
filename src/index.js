d1fcd81bf3295feb0345146ce23302ca40719262
sahil-sukhwani <sahil@shurutech.com>
2024-12-10 10:41:13 +0530

    added usePrevious hook & updated readme.md

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { useInterval } from "./hooks/useInterval";
 import { useTimeout } from "./hooks/useTimeout";
 import { useDebouncedValue } from "./hooks/useDebouncedValue";
+import { usePrevious } from "./hooks/usePrevious";
 
-export {useInterval,useTimeout,useDebouncedValue}
+export {useInterval,useTimeout,useDebouncedValue, usePrevious}
