a8b06ce6393cc0639927f75f7cdeee1836724a3a
dhruvshurutech <dhruv@shurutech.com>
2024-12-16 19:19:20 +0530

    exported function from root

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+export { useCopyToClipboard } from './hooks/useCopyToClipboard';
 export { useDebouncedValue } from "./hooks/useDebouncedValue";
 export { useInterval } from "./hooks/useInterval";
 export { useIsomorphicLayoutEffect } from "./hooks/useIsomorphicLayoutEffect";
